d8a9a9419cd8deb8e82d1b9a0bfc1775b09013c7
socomja <78257515+socomja@users.noreply.github.com>
2021-06-13 00:11:24 +0900

    socomja

--- commands/yyyyyy.js
+++ commands/yyyyyy.js
@@ -1,25 +1,47 @@
+const Discord = require('discord.js')
+const fetch = require('node-fetch')
+
+
 module.exports = {
-    name: "임베드1",
-    run: async (client, interaction, args) => {
-        const guild = client.guilds.cache.get(interaction.guild_id);
-        
+    name: 'yt',
+    description: 'Plays YT videos in voice channel',
+    usage: '<prefix>yt-together',
+    aliases: ['ytt'],
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+        const channel = message.member.voice.channel
 
-        if (!member.voice.channel) return client.sendTime(interaction, "❌ | You must be in a voice channel to use this command.");
-        if(!member.voice.channel.permissionsFor(guild.me).has("CREATE_INSTANT_INVITE"))return client.sendTime(interaction, "❌ | **Bot doesn't have Create Invite Permission**");
+        if(!channel) return message.channel.send(
+            new Discord.MessageEmbed()
+            .setDescription("You must be connected to a voice channel to use this command.")
+            .setColor("#ff0000")
+        )
 
-        let Invite = await member.voice.channel.activityInvite("755600276941176913")//Made using discordjs-activity package
-        let embed = new MessageEmbed()
-        .setAuthor("YouTube Together", "https://cdn.discordapp.com/emojis/749289646097432667.png?v=1")
-        .setColor("#FF0000")
-        .setDescription(`
-Using **YouTube Together** you can watch YouTube with your friends in a Voice Channel. Click *Join YouTube Together* to join in!
-
-__**[Join YouTube Together](https://discord.com/invite/${Invite.code})**__
-
-⚠ **Note:** This only works in Desktop
-`)
-        interaction.send(embed.toJSON())
-    },
+        fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+            method: "POST",
+            body: JSON.stringify({
+                max_age: 86400,
+                max_uses: 0,
+                target_application_id: "755600276941176913",
+                target_type: 2,
+                temporary : false,
+                validate: null
+            }),
+            headers: {
+                "Authorization": `Bot ${client.token}`,
+                "Content-Type": "application/json"
+            }
+        }).then(res => res.json()).then(invite => {
+            if(!invite.code) return message.channel.send(
+                new Discord.MessageEmbed()
+                .setDescription("I was unable to start a yt together session.")
+                .setColor("#ff0000")
+            )
+            message.channel.send(`Click This Link To Start a YouTube Together Session\nhttps://discord.com/invite/${invite.code}`)
+        })
+    }
 }
-
-
